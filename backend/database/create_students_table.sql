32e62ea714ec6a306707bc5d4691ad5452c1190c
SibPr <dengo123455@gmail.com>
2024-10-15 20:51:53 +0300

    Create tables for database

--- backend/database/create_students_table.sql
+++ backend/database/create_students_table.sql
@@ -1,9 +1,81 @@
-CREATE TABLE students (
-    id SERIAL PRIMARY KEY,
-    full_name VARCHAR(255),
-    student_ticket_number VARCHAR(50),
-    education_level VARCHAR(50),
-    department VARCHAR(100),
-    thesis_title VARCHAR(255),
-    is_archived BOOLEAN DEFAULT FALSE
+DROP TABLE IF EXISTS qualification_work;
+DROP TABLE IF EXISTS studies_in;
+DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS professor CASCADE;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS course;
+DROP TABLE IF EXISTS faculty;
+DROP TYPE IF EXISTS workstatus;
+DROP TYPE IF EXISTS worktype;
+DROP TYPE IF EXISTS educationstatus;
+DROP TYPE IF EXISTS educationlevel;
+
+CREATE TYPE educationlevel AS ENUM ('bachelor', 'master', 'postgraduate');
+-- bachelor - бакалавр
+-- master - магистр
+-- postgraduate - аспирант
+
+CREATE TYPE educationstatus AS ENUM ('student', 'graduate');
+-- student - является студентом в данный момент
+-- graduate - уже выпустился
+
+CREATE TYPE worktype AS ENUM ('course', 'diploma', 'master', 'dissertation');
+
+CREATE TYPE workstatus AS ENUM ('in progress', 'complete');
+
+
+CREATE TABLE IF NOT EXISTS faculty (
+	faculty_id serial PRIMARY KEY,
+	"name" character varying(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS course (
+    course_id smallserial PRIMARY KEY,
+	faculty_id integer NOT NULL REFERENCES faculty(faculty_id),
+    "name" character varying(200) NOT NULL,
+	education_level educationlevel NOT NULL
+);
+
+CREATE TABLE department (
+    department_id serial PRIMARY KEY,
+	faculty_id integer NOT NULL REFERENCES faculty(faculty_id),
+    "name" character varying(100) NOT NULL
+); -- далее еще добавлено поле department_head_id, см. ALTER TABLE
+
+CREATE TABLE professor (
+    professor_id serial PRIMARY KEY,
+	department_id integer REFERENCES department(department_id),
+    surname character varying(50) NOT NULL,
+    "name" character varying(50) NOT NULL,
+    patronymic character varying(50) NOT NULL
+);
+
+ALTER TABLE department
+ADD COLUMN department_head_id integer UNIQUE REFERENCES professor(professor_id);
+
+CREATE TABLE IF NOT EXISTS student (
+    student_card_id integer PRIMARY KEY,
+	department_id integer REFERENCES department(department_id),
+    surname character varying(50) NOT NULL,
+    "name" character varying(50) NOT NULL,
+    patronymic character varying(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS studies_in (
+	id serial PRIMARY KEY,
+	student_card_id integer NOT NULL REFERENCES student(student_card_id),
+	course_id smallserial NOT NULL REFERENCES course(course_id),
+	admission_year integer NOT NULL,
+	education_status educationstatus NOT NULL
+);
+
+CREATE TABLE qualification_work (
+    work_id serial PRIMARY KEY,
+	student_card_id integer NOT NULL REFERENCES student(student_card_id),
+	supervisor_id integer NOT NULL REFERENCES professor(professor_id),
+    "name" character varying(400) NOT NULL,
+    work_type worktype NOT NULL,
+	work_status workstatus NOT NULL,
+	grade integer CHECK (work_status = 'complete' AND grade IS NOT NULL 
+						 OR work_status = 'in progress' AND grade IS NULL)
 );
